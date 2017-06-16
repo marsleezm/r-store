628c750366c28cd777e28bcf3ccf215ba7e6c06c
marsleezm <mars.leezm@gmail.com>
2017-06-16 22:29:36 +0000

    Add stash to m

--- liscripts/remake.sh
+++ liscripts/remake.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./liscripts/parallel_command.sh "cd r-store && git pull && ant compile && ant hstore-prepare -Dproject=ycsb -Dhosts=local_ycsb.txt"
+./liscripts/parallel_command.sh "cd r-store && git stash && git pull && ant compile"
+#ant hstore-prepare -Dproject=ycsb -Dhosts=local_ycsb.txt
+
