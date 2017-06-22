04eab7e308518200030acc1b6cd8e5965208e1ca
Ubuntu <ubuntu@node01.novalocal>
2017-06-22 23:24:46 +0000

    Final remake

--- liscripts/remake.sh
+++ liscripts/remake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./liscripts/parallel_command.sh "`cat ./clients`" "cd r-store && git stash && git pull && ant compile" &
-./liscripts/parallel_command.sh "`cat ./nodes`" "cd r-store && git stash && git pull && ant compile"
+./liscripts/parallel_command.sh "`cat ./clients`" "cd r-store && git stash save -u && git pull && ant compile" &
+./liscripts/parallel_command.sh "`cat ./nodes`" "cd r-store && git stash save -u && git pull && ant compile"
 #ant hstore-prepare -Dproject=ycsb -Dhosts=local_ycsb.txt
 
