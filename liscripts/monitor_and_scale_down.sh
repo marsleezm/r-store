bc38bb3b0f8f49848e8174f89a50d11a11f560d9
Ubuntu <ubuntu@node01.novalocal>
2017-06-25 13:37:10 +0000

    Change to uniform

--- liscripts/monitor_and_scale_down.sh
+++ liscripts/monitor_and_scale_down.sh
@@ -18,8 +18,6 @@ do
     node_cnt=0
     for f in `ls $folder/*sarout*`
     do
-	if [ $node_cnt -lt $num_nodes ] 
-	then
 	    numcpu=`head -1 $f | awk -F '(' '{print $3}' | awk '{print $1}'`
             total_cpu=`bc <<< "$total_cpu + $numcpu" `
     	    idle=`tail -1 $f | awk -F ' ' '{print $9}'`
@@ -27,12 +25,11 @@ do
 	    c_usage=$(bc <<< "$usage * $numcpu")
 	    echo  "Usage is "$c_usage
 	    total_usage=$(bc <<< "$total_usage+$c_usage")
-	fi
-	node_cnt=$((node_cnt+1))
+		node_cnt=$((node_cnt+1))
     done
     th_usage=$((threshold * total_cpu))
     echo "Total is " $total_usage , th usage is $th_usage
-    larger=`echo $total_usage'>'$th_usage | bc -l`
+    larger=`echo $total_usage'<'$th_usage | bc -l`
     if [ $larger -eq 1 ]
     then
 	echo "Usage thresdhold hit, count is "$cnt
@@ -45,7 +42,7 @@ do
     if [ $cnt -ge $min_count ]
     then
 	echo "Decide to scale!" 
-	ant elastic-controller -Dproject=ycsb -DtWindow=18 -DnumPart=12  -DplannerID=1 -Dprovisioning=0 -DtimeLimit=5000 -Dglobal.hasher_plan=next_round.json -Dmonitoring=0 -DsitesPerHost=1 -DpartPerSite=$((12 / num_all_nodes)) -DhighCPU=160 -DlowCPU=110 -DchangeParts="+9;+10;+11" | tee $folder/controller_scale.out && cp plan_out.json next_round.json && ./liscripts/copy_to_all.sh "`cat ./nodes`" ./next_round.json
+	ant elastic-controller -Dproject=ycsb -DtWindow=12 -DnumPart=12  -DplannerID=1 -Dprovisioning=0 -DtimeLimit=5000 -Dglobal.hasher_plan=next_round.json -Dmonitoring=0 -DsitesPerHost=1 -DpartPerSite=$((12 / num_all_nodes)) -DhighCPU=160 -DlowCPU=110 -DchangeParts="-9;-10;-11" | tee $folder/controller_scale_down.out && cp plan_out.json next_round.json && ./liscripts/copy_to_all.sh "`cat ./nodes`" ./next_round.json
         break
     fi
     sleep $monitor_period
