ff3472fbea6554220978df622b8a7bd4c42c1a18
Ubuntu <ubuntu@node01.novalocal>
2017-07-11 00:53:10 +0000

    Add mot_reactive and proactive

---
+++ liscripts/mot_reactive.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+Period=5
+Duration=2400000
+ClientInt=$((Period*1000))
+OrgStatCnt=$((Duration / Period))
+OrgStatCnt=$((OrgStatCnt / 1000))
+UpCnt=10
+CPUTH=65
+DownCnt=20
+LowCPUTH=65
+StatCnt=$((OrgStatCnt+10))
+echo $ClientInt
+echo $StatCnt
+
+FirstNode=`head -1 ./nodes`
+#### Cleanup
+./liscripts/parallel_command.sh  "`cat ./nodes`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" & 
+./liscripts/parallel_command.sh  "`cat ./clients`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" &
+pkill -f ant
+pkill -f sar
+pkill -f monitor
+#rm hotTuplesPID*
+#rm siteLoadPID*
+./liscripts/parallel_command.sh "`cat ./nodes`" "pkill -f java" &
+./liscripts/parallel_command.sh "`cat ./nodes`" "pkill -f sar" &
+cp mot_target txnrates.txt
+./liscripts/copy_to_all.sh "`cat ./clients`" ./txnrates.txt ./r-store &
+for node in `cat ./nodes` 
+do
+    ssh ubuntu@$node 'cd r-store && rm -f output* && rm -f reconfig_output' &
+done
+#./liscripts/parallel_command.sh "`cat ./clients`" "cd r-store && mv txnrates.txt  txnrate.txt" &
+wait
+
+plan=$1
+
+### Make folder
+Date=`(date +'%Y%m%d-%H%M%S')`
+Folder="results/$Date"
+mkdir $Folder
+
+### Load the data store
+ssh ubuntu@$FirstNode "cd r-store && ant hstore-benchmark -Dproject=ycsb -Dglobal.hasher_plan=$plan -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Dnoshutdown=true -Dnoexecute=true -Dsite.txn_restart_limit_sysproc=100 -Dsite.jvm_asserts=false -Dsite.reconfig_live=true -Dsite.reconfig_async=true -Dsite.reconfig_async_pull=true | tee load_log" 1>$Folder/load_output 2>&1 &
+res=`grep "H-Store cluster remaining online until killed" $Folder/load_output`
+while [ "$res" == "" ]
+do 
+    echo "Wait for H-Store to finish loading"
+    sleep 5 
+    res=`grep "H-Store cluster remaining online until killed" $Folder/load_output`
+done
+echo H-Store finished loading
+
+./liscripts/monitor_cpu.sh $Folder $Period 
+
+InitT=40
+S1=$((InitT+33*60))
+sleep $S1 && ant elastic-controller -Dproject=ycsb -DtWindow=15 -DnumPart=10  -DplannerID=1 -Dprovisioning=0 -DtimeLimit=5000 -Dglobal.hasher_plan=next_round.json -Dmonitoring=0 -DsitesPerHost=1 -DpartPerSite=2 -DhighCPU=160 -DlowCPU=110 -DchangeParts=plan2_ycsb5.json &
+
+
+#ant hstore-benchmark -Dproject=ycsb -Dglobal.hasher_plan=$plan -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Dnostart=true -Dnoloader=true -Dnoshutdown=true -Dclient.txnrate=50 -Dclient.duration=$Duration -Dclient.interval=$ClientInt -Dclient.count=2 -Dclient.hosts="172.31.0.17;172.31.0.18" -Dclient.threads_per_host=16 -Dclient.blocking_concurrent=300 -Dclient.output_results_csv=$Folder/benchmark.csv -Dclient.output_interval=true
+ant hstore-benchmark -Dproject=ycsb -Dglobal.hasher_plan=$plan -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Dnostart=true -Dnoloader=true -Dnoshutdown=true -Dclient.txnrate=50 -Dclient.duration=$Duration -Dclient.interval=$ClientInt -Dclient.count=2 -Dclient.hosts="172.31.0.17;172.31.0.9" -Dclient.threads_per_host=16 -Dclient.blocking=false -Dclient.output_results_csv=$Folder/benchmark.csv -Dclient.output_interval=true
+
+pkill -f sar
+ 
+# Gather cpu usage
+./liscripts/process_cpu.sh $Folder $OrgStatCnt 
+
+# Calculate when reconfig ended
+scp ubuntu@$FirstNode:~/r-store/reconfig_output $Folder
+F=$Folder/reconfig_output
+#ReconfigStart=`head -1 $F | awk -F ':' '{print $2}'`
+#ReconfigEnd=`tail -2 $F | head -1 | awk -F ':' '{print $2}'`
+#echo $((ReconfigEnd-ReconfigStart)) >> $F
+#echo $ReconfigStart >> $F
+#echo $ReconfigEnd >> $F
