0a7b1fc536dc4d27d89042b6088eb2c089d1bc38
Ubuntu <ubuntu@store10.novalocal>
2018-04-04 09:50:40 +0000

    Stop using sar and ntp

--- liscripts/vadara_het.sh
+++ liscripts/vadara_het.sh
@@ -36,12 +36,12 @@ pkill -f sar
 pkill -f monitor
 pkill -f r-store
 pkill -f sleep
-./liscripts/parallel_command.sh  "`cat ./nodes`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" & 
-./liscripts/parallel_command.sh  "`cat ./clients`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" &
+#./liscripts/parallel_command.sh  "`cat ./nodes`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" & 
+#./liscripts/parallel_command.sh  "`cat ./clients`" "sudo /etc/init.d/ntp stop && sudo ntpdate ntp.ubuntu.com" &
 #rm hotTuplesPID*
 #rm siteLoadPID*
 ./liscripts/parallel_command.sh "`cat ./nodes`" "pkill -f java" &
-./liscripts/parallel_command.sh "`cat ./nodes`" "pkill -f sar" &
+#./liscripts/parallel_command.sh "`cat ./nodes`" "pkill -f sar" &
 ./liscripts/copy_to_all.sh "`cat ./clients`" ./txnrates.txt ./r-store &
 for node in `cat ./nodes` 
 do
@@ -79,7 +79,7 @@ do
 done
 echo H-Store finished loading
 
-./liscripts/monitor_cpu.sh $Folder $Period 
+#./liscripts/monitor_cpu.sh $Folder $Period 
 
 echo Plan1 will sleep $A1 && sleep $A1 && ant elastic-controller -Dproject=ycsb -DtWindow=15 -DnumPart=10  -DplannerID=1 -Dprovisioning=0 -DtimeLimit=5000 -Dglobal.hasher_plan=next_round.json -Dmonitoring=0 -DsitesPerHost=1 -DpartPerSite=2 -DhighCPU=160 -DlowCPU=110 -DchangeParts=plan2_vad.json  &
 echo Plan2 will sleep $A2 && sleep $A2 && ant elastic-controller -Dproject=ycsb -DtWindow=15 -DnumPart=10  -DplannerID=1 -Dprovisioning=0 -DtimeLimit=5000 -Dglobal.hasher_plan=next_round.json -Dmonitoring=0 -DsitesPerHost=1 -DpartPerSite=2 -DhighCPU=160 -DlowCPU=110 -DchangeParts=plan3_vad.json  &
@@ -100,10 +100,10 @@ echo Plan8 will sleep $R7 && sleep $R7 && ant elastic-controller -Dproject=ycsb 
 
 #ant hstore-benchmark -Dproject=ycsb -Dglobal.hasher_plan=$plan -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Dnostart=true -Dnoloader=true -Dnoshutdown=true -Dclient.txnrate=50 -Dclient.duration=$Duration -Dclient.interval=$ClientInt -Dclient.count=2 -Dclient.hosts="172.31.0.17;172.31.0.18" -Dclient.threads_per_host=16 -Dclient.blocking_concurrent=300 -Dclient.output_results_csv=$Folder/benchmark.csv -Dclient.output_interval=true
 ant hstore-benchmark -Dproject=ycsb -Dglobal.hasher_plan=$plan -Dglobal.hasher_class=edu.brown.hashing.TwoTieredRangeHasher -Dnostart=true -Dnoloader=true -Dnoshutdown=true -Dclient.txnrate=50 -Dclient.duration=$Duration -Dclient.interval=$ClientInt -Dclient.count=5 -Dclient.hosts="172.31.0.2;172.31.0.3;172.31.0.4;172.31.0.5;172.31.0.6" -Dclient.threads_per_host=8 -Dclient.blocking=false -Dclient.output_results_csv=$Folder/benchmark.csv -Dclient.output_interval=true | tee bench_out.txt
-pkill -f sar
+#pkill -f sar
  
 # Gather cpu usage
-./liscripts/process_cpu.sh $Folder $OrgStatCnt 
+#./liscripts/process_cpu.sh $Folder $OrgStatCnt 
 
 # Calculate when reconfig ended
 scp ubuntu@$FirstNode:~/r-store/reconfig_output $Folder
